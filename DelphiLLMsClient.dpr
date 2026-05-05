8c9dfec00ff90dc788cb3180b2e16f52d1c740ba
vettronics <oraitecamonyes@gmail.com>
2026-05-05 06:45:28 +0100

    Add Delphi VCL project entry point

---
+++ DelphiLLMsClient.dpr
@@ -0,0 +1,18 @@
+program DelphiLLMsClient;
+
+uses
+  Vcl.Forms,
+  MainForm in 'src\MainForm.pas',
+  LLM.Types in 'src\LLM.Types.pas',
+  LLM.Config in 'src\LLM.Config.pas',
+  LLM.ChatClients in 'src\LLM.ChatClients.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'Delphi LLMs Client';
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
