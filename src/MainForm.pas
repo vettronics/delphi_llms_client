388b6e5f3e66c352513a84e486940b5ba58e0c08
vettronics <oraitecamonyes@gmail.com>
2026-05-05 06:57:44 +0100

    Simplify chat form and expose OpenClaw session key

--- src/MainForm.pas
+++ src/MainForm.pas
@@ -3,46 +3,30 @@ unit MainForm;
 interface
 
 uses
-  System.SysUtils,
-  System.Classes,
-  System.Generics.Collections,
-  Vcl.Controls,
-  Vcl.Forms,
-  Vcl.StdCtrls,
-  Vcl.ExtCtrls,
-  Vcl.Dialogs,
-  LLM.Types,
-  LLM.Config,
-  LLM.ChatClients;
+  System.SysUtils, System.Classes, System.Generics.Collections,
+  Vcl.Controls, Vcl.Forms, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Dialogs,
+  LLM.Types, LLM.Config, LLM.ChatClients;
 
 type
   TfrmMain = class(TForm)
   private
     FHistory: TChatMessageList;
+    FChat: TMemo;
+    FPrompt: TMemo;
+    FProvider: TComboBox;
+    FModel: TEdit;
+    FBaseUrl: TEdit;
+    FSecret: TEdit;
+    FSessionKey: TEdit;
+    FKeepContext: TCheckBox;
+    FSend: TButton;
+    FClear: TButton;
+    FSave: TButton;
+    FSessionLabel: TLabel;
 
-    pnlTop: TPanel;
-    pnlBottom: TPanel;
-    pnlActions: TPanel;
-    memChat: TMemo;
-    memPrompt: TMemo;
-    cbProvider: TComboBox;
-    edtModel: TEdit;
-    edtBaseUrl: TEdit;
-    edtSecret: TEdit;
-    edtOpenClawEndpoint: TEdit;
-    chkKeepContext: TCheckBox;
-    btnSend: TButton;
-    btnClear: TButton;
-    btnSave: TButton;
-    lblProvider: TLabel;
-    lblModel: TLabel;
-    lblBaseUrl: TLabel;
-    lblSecret: TLabel;
-    lblEndpoint: TLabel;
-
-    procedure BuildInterface;
-    procedure LoadSettings;
-    procedure SaveSettings;
+    procedure BuildUi;
+    procedure LoadLocalSettings;
+    procedure SaveLocalSettings;
     procedure ProviderChanged(Sender: TObject);
     procedure SendClicked(Sender: TObject);
     procedure ClearClicked(Sender: TObject);
@@ -51,10 +35,10 @@ type
 
     function SelectedProvider: TLLMProvider;
     function CurrentSettings: TLLMSettings;
-    function BuildOutboundMessages(const AUserMessage: string): TChatMessageList;
-    procedure AppendChatLine(const APrefix, AText: string);
+    function BuildMessages(const AText: string): TChatMessageList;
+    procedure AddToChat(const ATitle, AText: string);
     procedure SetBusy(const AValue: Boolean);
-    procedure RefreshProviderUi;
+    procedure UpdateProviderUi;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -70,335 +54,232 @@ uses
 
 constructor TfrmMain.Create(AOwner: TComponent);
 begin
-  inherited Create(AOwner);
+  inherited;
   FHistory := TChatMessageList.Create;
-
   Caption := 'Delphi LLMs Client';
-  Width := 1150;
-  Height := 780;
+  Width := 1120;
+  Height := 760;
   Position := poScreenCenter;
   Font.Name := 'Segoe UI';
   Font.Size := 10;
-
-  BuildInterface;
-  LoadSettings;
+  BuildUi;
+  LoadLocalSettings;
 end;
 
 destructor TfrmMain.Destroy;
 begin
-  SaveSettings;
+  SaveLocalSettings;
   FHistory.Free;
   inherited;
 end;
 
-procedure TfrmMain.BuildInterface;
+procedure TfrmMain.BuildUi;
+var
+  TopPanel, BottomPanel, ButtonPanel: TPanel;
+  L: TLabel;
 begin
-  pnlTop := TPanel.Create(Self);
-  pnlTop.Parent := Self;
-  pnlTop.Align := alTop;
-  pnlTop.Height := 140;
-  pnlTop.BevelOuter := bvNone;
-  pnlTop.Padding.SetBounds(10, 10, 10, 8);
+  TopPanel := TPanel.Create(Self);
+  TopPanel.Parent := Self;
+  TopPanel.Align := alTop;
+  TopPanel.Height := 140;
+  TopPanel.BevelOuter := bvNone;
 
-  lblProvider := TLabel.Create(Self);
-  lblProvider.Parent := pnlTop;
-  lblProvider.Left := 12;
-  lblProvider.Top := 14;
-  lblProvider.Caption := 'Provider';
+  L := TLabel.Create(Self); L.Parent := TopPanel; L.SetBounds(12, 12, 120, 20); L.Caption := 'Provider';
+  FProvider := TComboBox.Create(Self);
+  FProvider.Parent := TopPanel;
+  FProvider.SetBounds(12, 34, 160, 24);
+  FProvider.Style := csDropDownList;
+  FProvider.Items.Add(ProviderToString(lpOpenAI));
+  FProvider.Items.Add(ProviderToString(lpGemini));
+  FProvider.Items.Add(ProviderToString(lpClaude));
+  FProvider.Items.Add(ProviderToString(lpGrok));
+  FProvider.Items.Add(ProviderToString(lpOpenRouter));
+  FProvider.Items.Add(ProviderToString(lpOpenClaw));
+  FProvider.OnChange := ProviderChanged;
 
-  cbProvider := TComboBox.Create(Self);
-  cbProvider.Parent := pnlTop;
-  cbProvider.Left := 12;
-  cbProvider.Top := 36;
-  cbProvider.Width := 160;
-  cbProvider.Style := csDropDownList;
-  cbProvider.Items.Add(ProviderToString(lpOpenAI));
-  cbProvider.Items.Add(ProviderToString(lpGemini));
-  cbProvider.Items.Add(ProviderToString(lpClaude));
-  cbProvider.Items.Add(ProviderToString(lpGrok));
-  cbProvider.Items.Add(ProviderToString(lpOpenRouter));
-  cbProvider.Items.Add(ProviderToString(lpOpenClaw));
-  cbProvider.OnChange := ProviderChanged;
+  L := TLabel.Create(Self); L.Parent := TopPanel; L.SetBounds(190, 12, 150, 20); L.Caption := 'Modelo / agente';
+  FModel := TEdit.Create(Self); FModel.Parent := TopPanel; FModel.SetBounds(190, 34, 245, 24);
 
-  lblModel := TLabel.Create(Self);
-  lblModel.Parent := pnlTop;
-  lblModel.Left := 190;
-  lblModel.Top := 14;
-  lblModel.Caption := 'Modelo / agente';
+  L := TLabel.Create(Self); L.Parent := TopPanel; L.SetBounds(455, 12, 120, 20); L.Caption := 'Base URL';
+  FBaseUrl := TEdit.Create(Self); FBaseUrl.Parent := TopPanel; FBaseUrl.SetBounds(455, 34, 330, 24);
 
-  edtModel := TEdit.Create(Self);
-  edtModel.Parent := pnlTop;
-  edtModel.Left := 190;
-  edtModel.Top := 36;
-  edtModel.Width := 240;
+  L := TLabel.Create(Self); L.Parent := TopPanel; L.SetBounds(805, 12, 120, 20); L.Caption := 'Chave';
+  FSecret := TEdit.Create(Self); FSecret.Parent := TopPanel; FSecret.SetBounds(805, 34, 295, 24); FSecret.PasswordChar := '*';
 
-  lblBaseUrl := TLabel.Create(Self);
-  lblBaseUrl.Parent := pnlTop;
-  lblBaseUrl.Left := 450;
-  lblBaseUrl.Top := 14;
-  lblBaseUrl.Caption := 'Base URL';
+  FSessionLabel := TLabel.Create(Self); FSessionLabel.Parent := TopPanel; FSessionLabel.SetBounds(12, 78, 180, 20); FSessionLabel.Caption := 'SessionKey OpenClaw';
+  FSessionKey := TEdit.Create(Self); FSessionKey.Parent := TopPanel; FSessionKey.SetBounds(12, 100, 260, 24);
 
-  edtBaseUrl := TEdit.Create(Self);
-  edtBaseUrl.Parent := pnlTop;
-  edtBaseUrl.Left := 450;
-  edtBaseUrl.Top := 36;
-  edtBaseUrl.Width := 330;
+  FKeepContext := TCheckBox.Create(Self);
+  FKeepContext.Parent := TopPanel;
+  FKeepContext.SetBounds(295, 101, 360, 24);
+  FKeepContext.Caption := 'Manter contexto local';
+  FKeepContext.Checked := True;
 
-  lblSecret := TLabel.Create(Self);
-  lblSecret.Parent := pnlTop;
-  lblSecret.Left := 800;
-  lblSecret.Top := 14;
-  lblSecret.Caption := 'API key / token';
+  FSave := TButton.Create(Self); FSave.Parent := TopPanel; FSave.SetBounds(680, 96, 110, 32); FSave.Caption := 'Guardar'; FSave.OnClick := SaveClicked;
+  FClear := TButton.Create(Self); FClear.Parent := TopPanel; FClear.SetBounds(800, 96, 140, 32); FClear.Caption := 'Limpar conversa'; FClear.OnClick := ClearClicked;
 
-  edtSecret := TEdit.Create(Self);
-  edtSecret.Parent := pnlTop;
-  edtSecret.Left := 800;
-  edtSecret.Top := 36;
-  edtSecret.Width := 310;
-  edtSecret.PasswordChar := '*';
+  FChat := TMemo.Create(Self);
+  FChat.Parent := Self;
+  FChat.Align := alClient;
+  FChat.ScrollBars := ssVertical;
+  FChat.ReadOnly := True;
+  FChat.WordWrap := True;
 
-  lblEndpoint := TLabel.Create(Self);
-  lblEndpoint.Parent := pnlTop;
-  lblEndpoint.Left := 12;
-  lblEndpoint.Top := 78;
-  lblEndpoint.Caption := 'Endpoint OpenClaw';
+  BottomPanel := TPanel.Create(Self);
+  BottomPanel.Parent := Self;
+  BottomPanel.Align := alBottom;
+  BottomPanel.Height := 160;
+  BottomPanel.BevelOuter := bvNone;
+  BottomPanel.Padding.SetBounds(10, 8, 10, 10);
 
-  edtOpenClawEndpoint := TEdit.Create(Self);
-  edtOpenClawEndpoint.Parent := pnlTop;
-  edtOpenClawEndpoint.Left := 12;
-  edtOpenClawEndpoint.Top := 100;
-  edtOpenClawEndpoint.Width := 260;
+  ButtonPanel := TPanel.Create(Self);
+  ButtonPanel.Parent := BottomPanel;
+  ButtonPanel.Align := alRight;
+  ButtonPanel.Width := 145;
+  ButtonPanel.BevelOuter := bvNone;
 
-  chkKeepContext := TCheckBox.Create(Self);
-  chkKeepContext.Parent := pnlTop;
-  chkKeepContext.Left := 295;
-  chkKeepContext.Top := 103;
-  chkKeepContext.Width := 220;
-  chkKeepContext.Caption := 'Manter contexto local';
-  chkKeepContext.Checked := True;
+  FSend := TButton.Create(Self);
+  FSend.Parent := ButtonPanel;
+  FSend.Align := alTop;
+  FSend.Height := 42;
+  FSend.Caption := 'Enviar';
+  FSend.Default := True;
+  FSend.OnClick := SendClicked;
 
-  btnSave := TButton.Create(Self);
-  btnSave.Parent := pnlTop;
-  btnSave.Left := 540;
-  btnSave.Top := 96;
-  btnSave.Width := 120;
-  btnSave.Caption := 'Guardar';
-  btnSave.OnClick := SaveClicked;
-
-  btnClear := TButton.Create(Self);
-  btnClear.Parent := pnlTop;
-  btnClear.Left := 670;
-  btnClear.Top := 96;
-  btnClear.Width := 140;
-  btnClear.Caption := 'Limpar conversa';
-  btnClear.OnClick := ClearClicked;
-
-  memChat := TMemo.Create(Self);
-  memChat.Parent := Self;
-  memChat.Align := alClient;
-  memChat.ScrollBars := ssVertical;
-  memChat.ReadOnly := True;
-  memChat.WordWrap := True;
-
-  pnlBottom := TPanel.Create(Self);
-  pnlBottom.Parent := Self;
-  pnlBottom.Align := alBottom;
-  pnlBottom.Height := 165;
-  pnlBottom.BevelOuter := bvNone;
-  pnlBottom.Padding.SetBounds(10, 8, 10, 10);
-
-  pnlActions := TPanel.Create(Self);
-  pnlActions.Parent := pnlBottom;
-  pnlActions.Align := alRight;
-  pnlActions.Width := 145;
-  pnlActions.BevelOuter := bvNone;
-
-  btnSend := TButton.Create(Self);
-  btnSend.Parent := pnlActions;
-  btnSend.Align := alTop;
-  btnSend.Height := 42;
-  btnSend.Caption := 'Enviar';
-  btnSend.Default := True;
-  btnSend.OnClick := SendClicked;
-
-  memPrompt := TMemo.Create(Self);
-  memPrompt.Parent := pnlBottom;
-  memPrompt.Align := alClient;
-  memPrompt.ScrollBars := ssVertical;
-  memPrompt.WordWrap := True;
-  memPrompt.OnKeyDown := PromptKeyDown;
+  FPrompt := TMemo.Create(Self);
+  FPrompt.Parent := BottomPanel;
+  FPrompt.Align := alClient;
+  FPrompt.ScrollBars := ssVertical;
+  FPrompt.WordWrap := True;
+  FPrompt.OnKeyDown := PromptKeyDown;
 end;
 
-procedure TfrmMain.LoadSettings;
+procedure TfrmMain.LoadLocalSettings;
 var
-  LSettings: TLLMSettings;
+  S: TLLMSettings;
 begin
-  LSettings := TLLMConfigStore.Load;
-
-  cbProvider.ItemIndex := Ord(LSettings.Provider);
-  edtModel.Text := LSettings.Model;
-  edtBaseUrl.Text := LSettings.BaseUrl;
-  edtOpenClawEndpoint.Text := LSettings.OpenClawEndpoint;
-  chkKeepContext.Checked := LSettings.KeepLocalContext;
-
-  if LSettings.Provider = lpOpenClaw then
-    edtSecret.Text := LSettings.OpenClawToken
-  else
-    edtSecret.Text := LSettings.ApiKey;
-
-  RefreshProviderUi;
+  S := TLLMConfigStore.Load;
+  FProvider.ItemIndex := Ord(S.Provider);
+  FModel.Text := S.Model;
+  FBaseUrl.Text := S.BaseUrl;
+  FSessionKey.Text := S.OpenClawEndpoint;
+  FKeepContext.Checked := S.KeepLocalContext;
+  if S.Provider = lpOpenClaw then FSecret.Text := S.OpenClawToken else FSecret.Text := S.ApiKey;
+  UpdateProviderUi;
 end;
 
-procedure TfrmMain.SaveSettings;
-var
-  LSettings: TLLMSettings;
+procedure TfrmMain.SaveLocalSettings;
 begin
-  LSettings := CurrentSettings;
-  TLLMConfigStore.Save(LSettings);
+  TLLMConfigStore.Save(CurrentSettings);
 end;
 
 function TfrmMain.SelectedProvider: TLLMProvider;
 begin
-  if cbProvider.ItemIndex < 0 then
-    Result := lpOpenAI
-  else
-    Result := TLLMProvider(cbProvider.ItemIndex);
+  if FProvider.ItemIndex < 0 then Result := lpOpenAI else Result := TLLMProvider(FProvider.ItemIndex);
 end;
 
 function TfrmMain.CurrentSettings: TLLMSettings;
 begin
   Result.Provider := SelectedProvider;
-  Result.Model := edtModel.Text.Trim;
-  Result.BaseUrl := edtBaseUrl.Text.Trim;
-  Result.KeepLocalContext := chkKeepContext.Checked;
-  Result.TimeoutSeconds := 120;
+  Result.Model := FModel.Text.Trim;
+  Result.BaseUrl := FBaseUrl.Text.Trim;
   Result.ApiKey := '';
   Result.OpenClawToken := '';
-  Result.OpenClawEndpoint := edtOpenClawEndpoint.Text.Trim;
+  Result.OpenClawEndpoint := FSessionKey.Text.Trim;
+  Result.KeepLocalContext := FKeepContext.Checked;
+  Result.TimeoutSeconds := 120;
 
-  if Result.Model = '' then
-    Result.Model := DefaultModel(Result.Provider);
-  if Result.BaseUrl = '' then
-    Result.BaseUrl := DefaultBaseUrl(Result.Provider);
-  if Result.OpenClawEndpoint = '' then
-    Result.OpenClawEndpoint := '/api/chat';
+  if Result.Model = '' then Result.Model := DefaultModel(Result.Provider);
+  if Result.BaseUrl = '' then Result.BaseUrl := DefaultBaseUrl(Result.Provider);
+  if Result.OpenClawEndpoint = '' then Result.OpenClawEndpoint := DefaultOpenClawSessionKey;
 
-  if Result.Provider = lpOpenClaw then
-    Result.OpenClawToken := edtSecret.Text.Trim
-  else
-    Result.ApiKey := edtSecret.Text.Trim;
+  if Result.Provider = lpOpenClaw then Result.OpenClawToken := FSecret.Text.Trim else Result.ApiKey := FSecret.Text.Trim;
 end;
 
 procedure TfrmMain.ProviderChanged(Sender: TObject);
 var
-  LProvider: TLLMProvider;
+  P: TLLMProvider;
 begin
-  LProvider := SelectedProvider;
-  edtModel.Text := DefaultModel(LProvider);
-  edtBaseUrl.Text := DefaultBaseUrl(LProvider);
-
-  if LProvider = lpOpenClaw then
-    edtOpenClawEndpoint.Text := '/api/chat';
-
-  RefreshProviderUi;
+  P := SelectedProvider;
+  FModel.Text := DefaultModel(P);
+  FBaseUrl.Text := DefaultBaseUrl(P);
+  if P = lpOpenClaw then FSessionKey.Text := DefaultOpenClawSessionKey;
+  UpdateProviderUi;
 end;
 
-procedure TfrmMain.RefreshProviderUi;
+procedure TfrmMain.UpdateProviderUi;
 var
-  LProvider: TLLMProvider;
+  IsOpenClaw: Boolean;
 begin
-  LProvider := SelectedProvider;
-
-  if LProvider = lpOpenClaw then
-    lblSecret.Caption := 'OpenClaw token'
-  else
-    lblSecret.Caption := 'API key';
-
-  lblEndpoint.Enabled := LProvider = lpOpenClaw;
-  edtOpenClawEndpoint.Enabled := LProvider = lpOpenClaw;
-
-  if LProvider = lpOpenClaw then
-    chkKeepContext.Caption := 'Manter contexto local (OpenClaw pode manter contexto no servidor)'
-  else
-    chkKeepContext.Caption := 'Manter contexto local';
+  IsOpenClaw := SelectedProvider = lpOpenClaw;
+  FSessionLabel.Enabled := IsOpenClaw;
+  FSessionKey.Enabled := IsOpenClaw;
+  if IsOpenClaw then FKeepContext.Caption := 'Manter contexto local; o gateway pode manter sessão própria' else FKeepContext.Caption := 'Manter contexto local';
 end;
 
-procedure TfrmMain.AppendChatLine(const APrefix, AText: string);
-begin
-  if memChat.Lines.Count > 0 then
-    memChat.Lines.Add('');
-  memChat.Lines.Add(APrefix + ':');
-  memChat.Lines.Add(AText);
-  memChat.SelStart := Length(memChat.Text);
-end;
-
-function TfrmMain.BuildOutboundMessages(const AUserMessage: string): TChatMessageList;
+function TfrmMain.BuildMessages(const AText: string): TChatMessageList;
 var
-  LMessage: TChatMessage;
+  M: TChatMessage;
 begin
   Result := TChatMessageList.Create;
+  if FKeepContext.Checked then for M in FHistory do Result.Add(M);
+  Result.Add(TChatMessage.Create('user', AText));
+end;
 
-  if chkKeepContext.Checked then
-  begin
-    for LMessage in FHistory do
-      Result.Add(LMessage);
-  end;
-
-  Result.Add(TChatMessage.Create('user', AUserMessage));
+procedure TfrmMain.AddToChat(const ATitle, AText: string);
+begin
+  if FChat.Lines.Count > 0 then FChat.Lines.Add('');
+  FChat.Lines.Add(ATitle + ':');
+  FChat.Lines.Add(AText);
+  FChat.SelStart := Length(FChat.Text);
 end;
 
 procedure TfrmMain.SendClicked(Sender: TObject);
 var
-  LUserMessage: string;
-  LSettings: TLLMSettings;
-  LClient: TCustomChatClient;
-  LOutbound: TChatMessageList;
-  LAnswer: string;
+  Msg, Answer: string;
+  C: TCustomChatClient;
+  OutMsgs: TChatMessageList;
+  S: TLLMSettings;
 begin
-  LUserMessage := memPrompt.Text.Trim;
-  if LUserMessage = '' then
-    Exit;
+  Msg := FPrompt.Text.Trim;
+  if Msg = '' then Exit;
 
-  SaveSettings;
-  LSettings := CurrentSettings;
-
-  AppendChatLine('Utilizador', LUserMessage);
-  memPrompt.Clear;
+  SaveLocalSettings;
+  S := CurrentSettings;
+  AddToChat('Utilizador', Msg);
+  FPrompt.Clear;
   SetBusy(True);
-
-  LOutbound := nil;
-  LClient := nil;
+  C := nil;
+  OutMsgs := nil;
   try
-    LOutbound := BuildOutboundMessages(LUserMessage);
-    LClient := TChatClientFactory.CreateClient(LSettings);
-    LAnswer := LClient.SendMessage(LOutbound);
-
-    FHistory.Add(TChatMessage.Create('user', LUserMessage));
-    FHistory.Add(TChatMessage.Create('assistant', LAnswer));
-    AppendChatLine(ProviderToString(LSettings.Provider), LAnswer);
+    OutMsgs := BuildMessages(Msg);
+    C := TChatClientFactory.CreateClient(S);
+    Answer := C.SendMessage(OutMsgs);
+    FHistory.Add(TChatMessage.Create('user', Msg));
+    FHistory.Add(TChatMessage.Create('assistant', Answer));
+    AddToChat(ProviderToString(S.Provider), Answer);
   except
     on E: Exception do
     begin
-      AppendChatLine('Erro', E.Message);
+      AddToChat('Erro', E.Message);
       MessageDlg(E.Message, mtError, [mbOK], 0);
     end;
   end;
-
-  LClient.Free;
-  LOutbound.Free;
+  C.Free;
+  OutMsgs.Free;
   SetBusy(False);
 end;
 
 procedure TfrmMain.ClearClicked(Sender: TObject);
 begin
   FHistory.Clear;
-  memChat.Clear;
-  memPrompt.SetFocus;
+  FChat.Clear;
+  FPrompt.SetFocus;
 end;
 
 procedure TfrmMain.SaveClicked(Sender: TObject);
 begin
-  SaveSettings;
+  SaveLocalSettings;
   MessageDlg('Definições guardadas.', mtInformation, [mbOK], 0);
 end;
 
@@ -413,20 +294,16 @@ end;
 
 procedure TfrmMain.SetBusy(const AValue: Boolean);
 begin
-  btnSend.Enabled := not AValue;
-  btnClear.Enabled := not AValue;
-  btnSave.Enabled := not AValue;
-  cbProvider.Enabled := not AValue;
-  edtModel.Enabled := not AValue;
-  edtBaseUrl.Enabled := not AValue;
-  edtSecret.Enabled := not AValue;
-  chkKeepContext.Enabled := not AValue;
-  edtOpenClawEndpoint.Enabled := (not AValue) and (SelectedProvider = lpOpenClaw);
-
-  if AValue then
-    Screen.Cursor := crHourGlass
-  else
-    Screen.Cursor := crDefault;
+  FSend.Enabled := not AValue;
+  FClear.Enabled := not AValue;
+  FSave.Enabled := not AValue;
+  FProvider.Enabled := not AValue;
+  FModel.Enabled := not AValue;
+  FBaseUrl.Enabled := not AValue;
+  FSecret.Enabled := not AValue;
+  FKeepContext.Enabled := not AValue;
+  FSessionKey.Enabled := (not AValue) and (SelectedProvider = lpOpenClaw);
+  if AValue then Screen.Cursor := crHourGlass else Screen.Cursor := crDefault;
   Application.ProcessMessages;
 end;
 
